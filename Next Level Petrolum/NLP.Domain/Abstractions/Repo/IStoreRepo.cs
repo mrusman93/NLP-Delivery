ae0ed4a4598241ca505812c6c12a85a94e626073
Muhammad Usman <usman.munawar@powersoft19.com>
2024-03-30 14:43:19 +0500

    StoreRepo

---
+++ Next Level Petrolum/NLP.Domain/Abstractions/Repo/IStoreRepo.cs
@@ -0,0 +1,19 @@
+﻿using NLP.Domain.Models;
+
+namespace NLP_Delivery_WebApplication.Abstractions.Repo
+{
+    public interface IStoreRepo
+    {
+        Task <List<Stores>> GetAllStoresAsync();
+        Task <Stores> GetStoreByIdAsync (int id);
+        Task <Stores> CreateStoreAsync (Stores store);
+        Task <Stores> UpdateStoreAsync (Stores updatedStore);
+        Task <Stores> DeleteStoreAsync (int id);
+        Task <List<Addresses>> ListStoreAddressAsync (int StoreId);
+        Task<Addresses> GetStoreAddressIdAsync (int StoreId, int AddressId);
+        Task<Addresses> CreateStoreAddressAsync(int StoreId, Addresses Address);
+        Task<Addresses> UpdateStoreAddressAsync(int StoreId, Addresses updatedAddress);
+        Task<Addresses> DeleteStoreAddressAsync(int StoreId, int AddressId);
+
+    }
+}
---
+++ Next Level Petrolum/NLP_Delivery_WebApplication/Abstractions/Repo/IStoreRepo.cs
@@ -0,0 +1,15 @@
+﻿using NLP.Domain.Models;
+
+namespace NLP_Delivery_WebApplication.Abstractions.Repo
+{
+    public interface IStoreRepo
+    {
+        List<Stores> GetAllStores();
+        Stores GetStoreById (int id);
+        Stores CreateStore(Stores store);
+        Stores UpdateStore(Stores updatedStore);
+        Stores DeleteStore(int id);
+        List<Addresses> ListStoreAddress(int StoreID);
+
+    }
+}
